99ecf96c1f8ff93aeffa98cf602c320ae8126e13
Mazedul Islam <mazidmailbox@gmail.com>
2023-06-10 12:23:11 +0600

    feat: installed chakra-ui

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,17 @@
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
+import App from "./App.tsx";
+import { theme } from "./theme.ts";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ChakraProvider theme={theme}>
+      <ColorModeScript initialColorMode={theme["config"].initialColorMode} />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ChakraProvider>
   </React.StrictMode>
 );
