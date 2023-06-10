e19d0deb5f9ff92cac54ba83332ca7de7be7c5d7
Mazedul Islam <mazidmailbox@gmail.com>
2023-06-11 02:22:30 +0600

    feat: update product

--- src/components/product/ProductsList.tsx
+++ src/components/product/ProductsList.tsx
@@ -1,8 +1,10 @@
-import { Spinner } from "@chakra-ui/react";
+import { Button, Spinner, useDisclosure } from "@chakra-ui/react";
 import { createColumnHelper } from "@tanstack/react-table";
+import { useState } from "react";
 import { Product } from "../../redux/api/@types";
 import { useGetAllProductsQuery } from "../../redux/api/productApiSlice";
 import { DataTable } from "../common/DataTable";
+import { ProductFormModal } from "./ProductFormModal";
 
 const { accessor: createColumn } = createColumnHelper<Product>();
 
@@ -14,8 +16,25 @@ export const ProductsList = () => {
     error,
   } = useGetAllProductsQuery();
 
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [clickedProduct, setClickedProduct] = useState<Product>();
+
   const columns = [
-    createColumn("name", { header: "Product Name" }),
+    createColumn("name", {
+      header: "Product Name",
+      cell: ({ row }) => (
+        <Button
+          colorScheme="teal"
+          variant="link"
+          onClick={() => {
+            setClickedProduct(row.original);
+            onOpen();
+          }}
+        >
+          {row.original.name}
+        </Button>
+      ),
+    }),
     createColumn("price", { header: "Unit Price" }),
     createColumn("status", { header: "Status" }),
     createColumn("availableSince", {
@@ -32,7 +51,18 @@ export const ProductsList = () => {
   ];
 
   if (isSuccess) {
-    return <DataTable columns={columns} data={products} />;
+    return (
+      <>
+        <DataTable columns={columns} data={products} />
+        <ProductFormModal
+          isOpen={isOpen}
+          onClose={onClose}
+          headerText="Add a new product"
+          saveButtonText="Save Product"
+          product={clickedProduct}
+        />
+      </>
+    );
   }
 
   if (isError) {
